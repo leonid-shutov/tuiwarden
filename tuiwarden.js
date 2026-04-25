eb90baaf5afa3029697136ce2d1df39c25fe37da
leonid-shutov <leonid.shutov@hey.com>
2026-04-25 22:05:46 +0200

    remove logs

--- tuiwarden.js
+++ tuiwarden.js
@@ -5,16 +5,6 @@ const blessed = require('blessed');
 const path = require('path');
 
 const rootDir = path.resolve(__dirname);
-
-process.on('unhandledRejection', (reason, promise) => {
-  console.dir({ promise });
-  console.dir({ reason });
-});
-
-process.on('uncaughtException', (...args) => {
-  console.dir({ args });
-});
-
 const context = { console, blessed, process, Buffer };
 
 uncommonjs.loadApplication(context, { rootDir });
